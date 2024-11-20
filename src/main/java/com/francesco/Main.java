b0689e84792c5da1f466d017ad46e0cecbb47915
francesco <f.debonis4@studenti.unisa.it>
2024-11-20 10:59:57 +0100

    commit catello

--- src/main/java/com/francesco/Main.java
+++ src/main/java/com/francesco/Main.java
@@ -3,5 +3,9 @@ package com.francesco;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Miao");//daje
+        System.out.println("Miaoooyygygoo");//daje
+        System.out.println("Modifica test2");//daje
+        System.out.println("Modifica test3");//catello
     }
 }
