6bd1ef239544159cd3783191293df7aea7e84d01
lucac <lucac@localhost>
2024-11-20 10:31:30 +0100

    secondo commit

--- src/main/java/com/francesco/Main.java
+++ src/main/java/com/francesco/Main.java
@@ -3,6 +3,6 @@ package com.francesco;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-                System.out.println("Miao");
+                System.out.println("Miao");//daje
     }
 }
