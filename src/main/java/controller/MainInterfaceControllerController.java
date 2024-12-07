0053478b8574c73f14d4d181c757f5dc2399ff7c
caty3 <caty3@LAPTOP-D03K2BFK>
2024-12-07 15:19:08 +0100

    pre doxygen

--- src/main/java/controller/MainInterfaceControllerController.java
+++ src/main/java/controller/MainInterfaceControllerController.java
@@ -46,7 +46,7 @@ public class MainInterfaceControllerController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
     } 
     @FXML
      public void esportaContatto(){
